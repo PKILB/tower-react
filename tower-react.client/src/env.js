3abba73e54fc91ceef50ce7b1378ae704af94a19
PKILB <paytonboone30@gmail.com>
2023-04-20 17:36:59 -0600

    back end and front end can be spun up now

--- tower-react.client/src/env.js
+++ tower-react.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'boone-coding.us.auth0.com'
+export const clientId = 'zucuSPuhK9rVj7G1FAciLHdEXaUb6yHb'
+export const audience = 'https://boone-coding.com'
