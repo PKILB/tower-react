d13ed2d4e74e8d2301f4f3221fb94b979bf9cf16
PKILB <paytonboone30@gmail.com>
2023-04-21 10:49:59 -0600

    event model is created

---
+++ tower-react/server/models/Event.js
@@ -0,0 +1,17 @@
+import { Schema } from "mongoose";
+
+
+
+export const EventSchema = new Schema({
+    name: { type: String, required: true, minLength: 5, maxLength: 50},
+    description: { type: String, required: true, minLength: 10, maxLength: 1000},
+    coverImg:  { type: String, required: true, maxLength: 5000},
+    location: { type: String, required: true, maxLength: 100},
+    capacity: { type: Number, required: true, min: 10, max: 20000},
+    startDate: { type: Date, required: true},
+    isCancelled: { type: Boolean, default: false, required: true},
+    type: { type: String, required: true, minLength: 3, maxLength: 500, enum: ['concert', 'convention', 'sport', 'digital']},
+
+    creatorId: { type: Schema.Types.ObjectId, ref: 'Account', required: true}
+}, { timestamps: true, toJSON: { virtuals: true }})
+
