55febdffc5b78f939d7acb77b9f4a1b64b2ef540
PKILB <paytonboone30@gmail.com>
2023-04-27 12:36:13 -0600

    create comment works

--- tower-react/server/services/CommentsService.js
+++ tower-react/server/services/CommentsService.js
@@ -1,9 +1,17 @@
-
+import { dbContext } from "../db/DbContext"
+import { BadRequest, Forbidden } from "../utils/Errors"
+import { eventsService } from "./EventsService"
 
 
 class CommentsService {
-    createComment(commentData) {
-        throw new Error("Method not implemented.");
+    async createComment(commentData) {
+        const event = await eventsService.getEventById(commentData.eventId)
+        if(event.isCanceled) {
+            throw new BadRequest('Event is Canceled')
+        }
+        const comment = await dbContext.Comments.create(commentData)
+        await comment.populate('creator', 'name picture')
+        return comment
     }
 
 }
