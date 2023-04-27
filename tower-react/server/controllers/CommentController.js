d4d874ac910b3d441cec79983a11a8dc7a1e6c0d
PKILB <paytonboone30@gmail.com>
2023-04-26 18:00:28 -0600

    create comment done in controller

--- tower-react/server/controllers/CommentController.js
+++ tower-react/server/controllers/CommentController.js
@@ -1,5 +1,6 @@
 import { Auth0Provider } from "@bcwdev/auth0provider";
 import BaseController from "../utils/BaseController";
+import { commentsService } from "../services/CommentsService";
 
 
 
@@ -10,8 +11,15 @@ export class CommentsController extends BaseController {
         .use(Auth0Provider.getAuthorizedUserInfo)
         .post('', this.createComment)
     }
-    createComment(arg0, createComment) {
-        throw new Error("Method not implemented.");
+    async createComment(req, res, next) {
+        try {
+            const commentData = req.body
+            commentData.creatorId = req.userInfo.id
+            const comment = await commentsService.createComment(commentData)
+            return res.send(comment)
+        } catch (error) {
+            next(error)
+        }
     }
 
     
---
+++ tower-react/server/services/CommentsService.js
@@ -0,0 +1,11 @@
+
+
+
+class CommentsService {
+    createComment(commentData) {
+        throw new Error("Method not implemented.");
+    }
+
+}
+
+export const commentsService = new CommentsService()
