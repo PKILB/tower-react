90564b14a82efa07d971f08cb76ba5870b3dd885
PKILB <paytonboone30@gmail.com>
2023-04-27 12:43:44 -0600

    delete comment done in controller

--- tower-react/server/services/CommentsService.js
+++ tower-react/server/services/CommentsService.js
@@ -4,6 +4,13 @@ import { eventsService } from "./EventsService"
 
 
 class CommentsService {
+    deleteComment(commentId, requestorId) {
+        throw new Error("Method not implemented.")
+    }
+    async getEventComments(eventId) {
+        const comments = await dbContext.Comments.find({ eventId }).populate('creator', 'name picture')
+        return comments
+    }
     async createComment(commentData) {
         const event = await eventsService.getEventById(commentData.eventId)
         if(event.isCanceled) {
