3bcbc0a42a2e0f3c3a648532c50abc7ebf2b5c07
PKILB <paytonboone30@gmail.com>
2023-05-01 08:55:33 -0600

    all cards have a fixed height now

--- tower-react.client/src/components/EventCard.jsx
+++ tower-react.client/src/components/EventCard.jsx
@@ -1,14 +1,15 @@
 import { Event } from "../models/Event";
 import React from "react";
 import PropTypes from "prop-types";
+import './styles/EventCard.scss';
 
 
 /**@param {{event:Event}} props */
 export default function EventCard({event}) {
 
     return (
-        <div className="card">
-            <img className="img-fluid" src={event.coverImg} alt="" />
+        <div className="card event-card" >
+            <img className="img-fluid" src={event.coverImg} alt=""/>
         </div>
     )
 }
