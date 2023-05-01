1d90c1e8c79dd6ac9108074c33d0bee7c1b67a07
PKILB <paytonboone30@gmail.com>
2023-05-01 12:29:32 -0600

    glass effect added to event cards

--- tower-react.client/src/components/EventCard.jsx
+++ tower-react.client/src/components/EventCard.jsx
@@ -10,11 +10,11 @@ export default function EventCard({event}) {
     return (
         <div className="card event-card my-3 bg-dark elevation-5" >
             <img className="img-fluid" src={event.coverImg} alt=""/>
-            <div className="card-img-overlay row">
-                <div>
+            <div className="card-img-overlay event-card-content row d-flex align-content-end">
+                <div className="">
                     <h5>{event.name}</h5>
                     <h6>{event.location}</h6>
-                    <p>{event.capacity} spots left</p>
+                    <p className="d-flex justify-content-end">{event.capacity} spots left</p>
                 </div>
             </div>
         </div>
