865fc8d0bb98ec66e017cdc189cda6ef4f03a4bc
PKILB <paytonboone30@gmail.com>
2023-05-01 14:15:18 -0600

    more styling added

--- tower-react.client/src/components/EventCard.jsx
+++ tower-react.client/src/components/EventCard.jsx
@@ -13,8 +13,8 @@ export default function EventCard({event}) {
             <div className="card-img-overlay event-card-content row d-flex align-content-end">
                 <div className="">
                     <h5>{event.name}</h5>
-                    <h6>{event.location}</h6>
-                    <p className="d-flex justify-content-end">{event.capacity} spots left</p>
+                    <h6 className="text-grey">{event.location}</h6>
+                    <p className="d-flex justify-content-end text-grey">{event.capacity} spots left</p>
                 </div>
             </div>
         </div>
