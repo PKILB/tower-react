9fd57e480f63908b5c9e6c2728e31439a572a4d9
PKILB <paytonboone30@gmail.com>
2023-05-01 12:06:04 -0600

    card img overlay works, design next

--- tower-react.client/src/components/EventCard.jsx
+++ tower-react.client/src/components/EventCard.jsx
@@ -10,6 +10,13 @@ export default function EventCard({event}) {
     return (
         <div className="card event-card my-3 bg-dark elevation-5" >
             <img className="img-fluid" src={event.coverImg} alt=""/>
+            <div className="card-img-overlay row">
+                <div>
+                    <h5>{event.name}</h5>
+                    <h6>{event.location}</h6>
+                    <p>{event.capacity} spots left</p>
+                </div>
+            </div>
         </div>
     )
 }
